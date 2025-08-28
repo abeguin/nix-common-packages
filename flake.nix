c4ee2f3afc01cd0322cf08f7d8e75486114d67fc
Johan Lanzrein <jlanzrein@pictet.com>
2025-08-28 16:12:24 +0200

    fix: typo in system x86_64_linux

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
   outputs = { self, ... }@inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64_linux" "x86_64-darwin" "aarch64-darwin" ];
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
       perSystem = { system, pkgs, ... }:
         let
